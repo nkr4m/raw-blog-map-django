e3937ea23203f0a756757d246eec5b9d151f2d61
N.K Ramachandran <nnkrama11@gmail.com>
2020-09-30 21:16:37 +0530

    raw

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from .import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'), 
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+]
+
+
+
